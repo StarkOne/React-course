7925722aaa7f3bb0b0b2f7c98eb56a46f87de784
StarkOne <Stark.pro40@gmail.com>
2018-02-17 14:06:06 +0700

    React events

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import '../css/style.css';
 
-class Header extends Component {
-  render() {
 
+class Header extends Component {
+  inputChangeHandler(event) {
+    console.log(event.target.value);
+  }
+  render() {
     return <header>
         <div className="logo">logo</div>
-        <input type="text" />
+        <input 
+          type="text" 
+          onChange={ (e) => this.inputChangeHandler(e) } 
+      />
       </header>;
   }
 }
