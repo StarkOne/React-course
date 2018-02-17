ff917c2faa4176bc378324a0ea3a120078ef3438
StarkOne <Stark.pro40@gmail.com>
2018-02-17 12:46:20 +0700

    css React

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
+import '../css/style.css';
 
 class Header extends Component {
   render() {
-    return (
-      <header>
-        <div>logo</div>
+
+    return <header>
+        <div className="logo">logo</div>
         <input type="text" />
-      </header>
-    );
+      </header>;
   }
 }
 
