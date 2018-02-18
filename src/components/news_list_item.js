fcf981e81cb9fd76b2ce1b1abf4588563e34bc1b
StarkOne <Stark.pro40@gmail.com>
2018-02-18 08:37:44 +0700

    React css mod

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,29 +1,12 @@
 import React from 'react';
-import { css } from "glamor";
+import classes from '../css/style.css';
 
 const NewsItem = ({item}) => {
-  let news_item = css({
-      padding: '20px',
-      boxSizing: 'border-box',
-      borderBottom: '1px solid grey',
-      ':hover': {
-        color: 'red'
-      },
-      '@media(max-width: 500px)': {
-        color : "blue"
-      }
-  });
-
-  let item_grey = css({
-    background: 'lightgrey'
-  })
 
   return (
-    <div className = {`${news_item} ${item_grey}`}>
-      <div>
+    <div className={classes.news_item}>
         <h3>{item.title}</h3>
         <div>{item.feed}</div>
-      </div>
     </div>
   )
 }
